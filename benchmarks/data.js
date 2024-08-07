35985c07fc023550453cd357f237f7267ea02108
github-action-benchmark <github@users.noreply.github.com>
2024-08-07 21:42:11 +0000

    add OpenTelemetry-cpp exporters Benchmark (googlecpp) benchmark result for 7c6b9dbcaf43af9a281029121392aef31445b770

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723066866104,
+  "lastUpdate": 1723066931818,
   "repoUrl": "https://github.com/marcalff/opentelemetry-cpp",
   "entries": {
     "OpenTelemetry-cpp api Benchmark": [
@@ -84771,6 +84771,54 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/marcalff/opentelemetry-cpp/commit/34ca855055ebb9804756ff220fd64cb45d0c6de5"
         },
         "date": 1722708794397,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_OtlpExporterEmptySpans",
+            "value": 17.927679155494392,
+            "unit": "ns/iter",
+            "extra": "iterations: 7845000\ncpu: 17.9238535372849 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_OtlpExporterSparseSpans",
+            "value": 132.72675154023676,
+            "unit": "ns/iter",
+            "extra": "iterations: 1067000\ncpu: 130.82944048734774 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_OtlpExporterDenseSpans",
+            "value": 944.7481992433397,
+            "unit": "ns/iter",
+            "extra": "iterations: 139000\ncpu: 940.8288561151079 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_otlp_grpc_with_collector",
+            "value": 1623670.6026669205,
+            "unit": "ns/iter",
+            "extra": "iterations: 232\ncpu: 605454.9612068967 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "BYVoid@users.noreply.github.com",
+            "name": "Carbo Kuo",
+            "username": "BYVoid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7c6b9dbcaf43af9a281029121392aef31445b770",
+          "message": "[BUILD] Add abi_version_no bazel flag. (#3020)",
+          "timestamp": "2024-08-06T19:43:02+02:00",
+          "tree_id": "3f1016608a8f87b167e7b54ff16f88e0110c6e91",
+          "url": "https://github.com/marcalff/opentelemetry-cpp/commit/7c6b9dbcaf43af9a281029121392aef31445b770"
+        },
+        "date": 1723066928448,
         "tool": "googlecpp",
         "benches": [
           {
